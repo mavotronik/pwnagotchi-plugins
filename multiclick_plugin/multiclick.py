74a2a2757448aea96c0b63802f1ebb0425cc1df4
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-07-05 21:58:30 +0300

    Add files via upload

---
+++ multiclick_plugin/multiclick.py
@@ -0,0 +1,105 @@
+import pwnagotchi.plugins as plugins
+import pwnagotchi.ui as ui
+import pwnagotchi
+import os
+import json
+import logging
+from gpiozero import Button
+
+held_for = 0.0
+click = 0
+hold = False
+
+gpioPin = 21
+commands = 4
+hold_tme = 4.0
+
+#modeSwitch = 'AUTO'
+#modeNow = 'MANU'
+
+button = Button(gpioPin, hold_time=0.5, hold_repeat=False)
+
+class multiclck(plugins.Plugin):
+
+    __author__ = 'mavotronik'
+    __version__ = '1.0.0'
+    __license__ = 'GPL3'
+    __description__ = 'any disription'
+
+                
+    def on_loaded(self):
+        logging.info("Multiclck: loaded")
+        
+    def on_ready(self, agent):
+        global held_for
+        global click
+        
+        click = 0
+        held_for = 1
+        
+        logging.info("Multiclck: waiting for button")
+    
+    def onclick(self):
+        global click
+        global commands
+        click = click + 1
+        if (click > commands):
+            click = 1
+            
+    def onheld(self):
+        global held_for
+        global hold_tme
+        global hold
+        held_for = max(held_for, button.held_time + button.hold_time + 1)
+        if (held_for >= hold_tme):
+            hold = True
+            logging.info("Multiclck: button hold")
+        else:
+            hold = False
+        
+
+    def run_command(self):
+        global hold
+        global click
+        if (hold == True):
+           if (click == 1):
+                logging.info("Multiclck: 1 mode")
+           elif (click == 2):
+                logging.info("Multiclck: 2 mode")
+           elif (click == 3):
+                logging.info("Multiclck: 3 mode")
+           elif (click == 4):
+                logging.info("Multiclck: 4 mode")
+           else:
+                logging.info("Multiclck: empty mode")
+                
+          
+    def on_ui_setup(self, ui):
+        global hold
+        global click
+        ui.add_element('multiclckStatus',
+                        LabeledValue(
+                        color=BLACK,
+                        label='MCS',
+                        value='-',
+                        position=(ui.width() - 42, ui.height() - 13),
+                        label_font=fonts.Bold,
+                        text_font=fonts.Medium
+                        ))
+
+    def on_ui_update(self, ui):
+        global click
+        ui.set('multiclckStatus', click)
+
+        
+    def on_unloaded(self):
+        global button
+        button.close()
+        logging.info("Multiclck: unloaded")
+        ui.remove_element('multiclckStatus')
+
+    
+#    button.is_pressed = onclick    
+    button.when_pressed = onclick
+    button.when_held = onheld
+    button.when_released = run_command
