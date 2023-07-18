a414a45239556b72eceb47b3c730815b6b6d5d69
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-07-18 18:28:25 +0300

    Update clockie.py

--- clockie/clockie.py
+++ clockie/clockie.py
@@ -11,14 +11,10 @@ import paho.mqtt.client as mqtt
 
 class clockie(plugins.Plugin):
     __author__ = 'https://github.com/mavotronik/pwnagotchi-plugins'
-    __version__ = '0.0.3'
+    __version__ = '0.0.4'
     __license__ = 'MIT'
     __description__ = 'A plugin displays a clock and other info'
     
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect("localhost", 1883, 60)
-
     def on_loaded(self):
         logging.info("clockie: plugin loaded")
         
@@ -27,20 +23,48 @@ class clockie(plugins.Plugin):
         
         
     def on_ui_setup(self, ui):
-        ui.add_element('clockie_w', Text(color=BLACK, value='c',position=(ui.width() - 42, ui.height() - 13), font=fonts.Medium))
-        ui.add_element('clock', Text(color=BLACK, value='',position=(130, 80), font=fonts.Medium))
-        
+        # ui.add_element('clockie_w', Text(color=BLACK, value='c',position=(ui.width() - 42, ui.height() - 13), font=fonts.Medium))
+        ui.add_element('clock', Text(color=BLACK, value='',position=(150, 56), font=fonts.Medium))
+        ui.add_element('clock_mqtt_1', Text(color=BLACK, value='',position=(145, 85), font=fonts.Medium))
+        ui.add_element('clock_mqtt_2', Text(color=BLACK, value='',position=(175, 85), font=fonts.Medium))
+                
     def on_ui_update(self, ui):
         dt_now = datetime.datetime.now()
-        time_rn = dt_now.strftime("%d/%m/%y" + "\n%H:%M:%S")
+        # time_rn = dt_now.strftime("%d/%m/%y" + "\n%H:%M:%S")  #with seconds
+        time_rn = dt_now.strftime("%d/%m/%y" + "\n%H:%M")   #no seconds
         ui.set('clock', time_rn)
-            
+        ui.set('clock_mqtt_1', "#")
+        ui.set('clock_mqtt_2', "#")
+        # remove_all()
+        with ui._lock:
+            # ui.remove_element('bluetooth')
+            ui.remove_element('memtemp')
+            ui.remove_element('latitude')
+            ui.remove_element('longitude')
+            ui.remove_element('altitude')
+            # ui.remove_element('aps')
+            # ui.remove_element('channel')
+            # ui.remove_element('uptime')
         
-            
           
     def on_unload(self, ui):
         logging.info("clockie: plugin unloaded")
         with ui._lock:
             ui.remove_element('clock')
-            ui.remove_element('clockie_w')
+            ui.remove_element('clock_mqtt_1')
+            ui.remove_element('clock_mqtt_2')
+            # ui.remove_element('clockie_w')
+            
+    # def remove_all(ui):
+        # logging.info("clockie: removing all")
+        # with ui._lock:
+            # # ui.remove_element('bluetooth')
+            # ui.remove_element('memtemp')
+            # ui.remove_element('latitude')
+            # ui.remove_element('longitude')
+            # ui.remove_element('altitude')
+            # # ui.remove_element('aps')
+            # # ui.remove_element('channel')
+            # # ui.remove_element('uptime')
+            
             
