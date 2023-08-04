1963fb9217258c9df70edb568bd8b3f77e10f672
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-08-04 19:45:43 +0300

    Update clockie.py

--- clockie/clockie.py
+++ clockie/clockie.py
@@ -11,9 +11,13 @@ import paho.mqtt.client as mqtt
 
 class clockie(plugins.Plugin):
     __author__ = 'https://github.com/mavotronik/pwnagotchi-plugins'
-    __version__ = '0.0.4'
+    __version__ = '0.1.0'
     __license__ = 'MIT'
     __description__ = 'A plugin displays a clock and other info'
+    
+    def __init__(self): 
+        self.client = mqtt.Client()
+        self.client.connect("localhost", 1883, 60)    
     
     def on_loaded(self):
         logging.info("clockie: plugin loaded")
@@ -23,28 +27,28 @@ class clockie(plugins.Plugin):
         
         
     def on_ui_setup(self, ui):
-        # ui.add_element('clockie_w', Text(color=BLACK, value='c',position=(ui.width() - 42, ui.height() - 13), font=fonts.Medium))
+        # ui.add_element('clockie_w', Text(color=BLACK, value='c',position=(ui.width() - 42, ui.height() - 16), font=fonts.Medium))
         ui.add_element('clock', Text(color=BLACK, value='',position=(150, 56), font=fonts.Medium))
-        ui.add_element('clock_mqtt_1', Text(color=BLACK, value='',position=(145, 85), font=fonts.Medium))
-        ui.add_element('clock_mqtt_2', Text(color=BLACK, value='',position=(175, 85), font=fonts.Medium))
+        ui.add_element('clock_mqtt_1', Text(color=BLACK, value='m',position=(145, 85), font=fonts.Medium))
+        ui.add_element('clock_mqtt_2', Text(color=BLACK, value='m',position=(175, 85), font=fonts.Medium))
                 
     def on_ui_update(self, ui):
         dt_now = datetime.datetime.now()
         # time_rn = dt_now.strftime("%d/%m/%y" + "\n%H:%M:%S")  #with seconds
         time_rn = dt_now.strftime("%d/%m/%y" + "\n%H:%M")   #no seconds
         ui.set('clock', time_rn)
-        ui.set('clock_mqtt_1', "#")
-        ui.set('clock_mqtt_2', "#")
+        
+        clockie_1 = self.client.subscribe("pwnagotchi/clockie_1")
+        clockie_2 = self.client.subscribe("pwnagotchi/clockie_2")
+        ui.set('clock_mqtt_1', clockie_1)
+        ui.set('clock_mqtt_2', clockie_2)
         # remove_all()
         with ui._lock:
-            # ui.remove_element('bluetooth')
             ui.remove_element('memtemp')
             ui.remove_element('latitude')
             ui.remove_element('longitude')
             ui.remove_element('altitude')
-            # ui.remove_element('aps')
-            # ui.remove_element('channel')
-            # ui.remove_element('uptime')
+            
         
           
     def on_unload(self, ui):
@@ -54,17 +58,4 @@ class clockie(plugins.Plugin):
             ui.remove_element('clock_mqtt_1')
             ui.remove_element('clock_mqtt_2')
             # ui.remove_element('clockie_w')
-            
-    # def remove_all(ui):
-        # logging.info("clockie: removing all")
-        # with ui._lock:
-            # # ui.remove_element('bluetooth')
-            # ui.remove_element('memtemp')
-            # ui.remove_element('latitude')
-            # ui.remove_element('longitude')
-            # ui.remove_element('altitude')
-            # # ui.remove_element('aps')
-            # # ui.remove_element('channel')
-            # # ui.remove_element('uptime')
-            
             
