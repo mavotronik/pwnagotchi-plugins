e81c9c60ef1452eb4d96ad58823c73810fa822a2
Matvey <66329687+mavotronik@users.noreply.github.com>
2024-06-29 00:59:32 +0300

    Update hcxpcapngtool.py

--- hcxpcapngtool/hcxpcapngtool.py
+++ hcxpcapngtool/hcxpcapngtool.py
@@ -1,1 +1,22 @@
+import pwnagotchi.plugins as plugins
+#import pwnagotchi.ui as ui
+import pwnagotchi
+import os
+import json
+import logging
 
+class Hcxpcapngtool(plugins.Plugin):
+
+    __author__ = 'https://github.com/mavotronik/pwnagotchi-plugins'
+    __version__ = '1.0.0'
+    __license__ = 'MIT'
+    __description__ = 'A plugin that converts .pcap files to .hc22000 files'
+
+    def on_loaded(self):
+        logging.info("Hcxpcapngtool Plugin: loaded")
+
+    def on_unloaded(self):
+        logging.info("Hcxpcapngtool Plugin: unloaded")
+
+    def on_handshake(self, agent, filename, access_point, client_station):
+        logging.info("Hcxpcapngtool Plugin: New handshake" + filename + "captured!")
