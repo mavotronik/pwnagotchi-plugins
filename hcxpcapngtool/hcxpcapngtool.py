8ae4c2ef78f1f3204d8c075a8fe89da1907ae259
Matvey <66329687+mavotronik@users.noreply.github.com>
2024-06-29 01:57:17 +0300

    Add check and create final directory

--- hcxpcapngtool/hcxpcapngtool.py
+++ hcxpcapngtool/hcxpcapngtool.py
@@ -2,8 +2,8 @@ import pwnagotchi.plugins as plugins
 #import pwnagotchi.ui as ui
 import pwnagotchi
 import os
-import json
 import logging
+import subprocess
 
 class Hcxpcapngtool(plugins.Plugin):
 
@@ -12,11 +12,20 @@ class Hcxpcapngtool(plugins.Plugin):
     __license__ = 'MIT'
     __description__ = 'A plugin that converts .pcap files to .hc22000 files'
 
+    def __init__(self):
+        self.text_to_set = ""
+    
     def on_loaded(self):
         logging.info("Hcxpcapngtool Plugin: loaded")
+        if os.path.exists(self.options['hc_folder']):
+            print("Hcxpcapngtool Plugin: hc directory is ok")
+        else:
+            print("Hcxpcapngtool Plugin: Creating hc dierectory . . .")
+            subprocess.run(('sudo mkdir ' + self.options['hc_folder']), shell=True, stdout=subprocess.PIPE)
 
     def on_unloaded(self):
         logging.info("Hcxpcapngtool Plugin: unloaded")
 
     def on_handshake(self, agent, filename, access_point, client_station):
-        logging.info("Hcxpcapngtool Plugin: New handshake" + filename + "captured!")
+        logging.info("Hcxpcapngtool Plugin: New handshake " + filename + " captured!")
+        
