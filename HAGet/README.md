1d4012854d93257cd8142b8b7d9e745d856370bb
Mavotronik <matveypopov2004@gmail.com>
2025-03-20 22:32:40 +0300

    PreCreated new plugin. HAGet

---
+++ HAGet/README.md
@@ -0,0 +1,20 @@
+
+# Plugin Setup
+1. If you have not set up a directory for custom plugins, create the directory and add its path to your config.toml.
+`main.custom_plugins = "/usr/local/share/pwnagotchi/custom-plugins/"`
+2. Install dependencies
+3. Put plugin to your custom folder
+4. Edit your config.toml
+5. Restart pwnagotchi service
+
+## WARNING! PROJECT IS UNDER CONSTRUCTION
+
+# HAGet
+- Displays any data from your Home Assistant
+- Tested on 1.5.5
+
+# Dependencies
+- TBD
+
+# Config.toml
+- TBD
---
+++ HAGet/haget.py
@@ -0,0 +1,36 @@
+from pwnagotchi.ui.components import LabeledValue, Text
+from pwnagotchi.ui.view import BLACK
+import pwnagotchi.ui.fonts as fonts
+import pwnagotchi.plugins as plugins
+import pwnagotchi
+import logging
+import requests
+
+URL = ""
+TOKEN = ""
+ENTITIES = ""
+
+
+######  PROJECT IS UNDER CONSTRUCTOIN!!!!!!! ##########
+
+class HaGet(plugins.Plugin):
+    __author__ = 'https://github.com/mavotronik/pwnagotchi-plugins'
+    __version__ = '1.0.0'
+    __license__ = 'MIT'
+    __description__ = 'A plugin that will get any data from your Home Assistant'
+
+    def on_loaded(self):
+        self.url = self.options.get('url', 'NOT SET')
+        self.token = self.options.get('token', 'NOT SET')
+        self.entities = self.options.get('entities', [])
+
+        if isinstance(self.entities, str):
+            self.entities = [self.entities]
+
+        logging.info(f"HAGet: plugin loaded with settings: URL={self.url}, TOKEN={self.token}, ENTITIES={', '.join(self.entities)}")
+
+    def on_unloaded(self):
+        logging.info("HAGet: plugin unloaded.")
+
+    def on_ui_update(self, ui):
+        logging.info("HAGet: ui updated.")
